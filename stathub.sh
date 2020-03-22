0c10451d8dbd019388cc7939e96333a26181f033
3136352472 <3136352472@qq.com>
2020-03-22 22:33:03 +0800

    `update`

--- stathub.sh
+++ stathub.sh
@@ -1,3 +1,4 @@
+rm -rf /root/hasaki/stathub
 mkdir -p /root/hasaki/stathub
 wget https://service-1253872964.cos.ap-guangzhou.myqcloud.com/stathub -P /root/hasaki/stathub/
 chmod 755 /root/hasaki/stathub/stathub
