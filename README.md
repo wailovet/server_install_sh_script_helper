5142a131a2e72fd92e915816bff753033e4378b5
3136352472 <3136352472@qq.com>
2020-03-22 22:32:05 +0800

    `update`

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # server_install_sh_script_helper
+
+>curl https://wailovet.github.io/server_install_sh_script_helper/xxx.sh | bash
--- stathub.sh
+++ stathub.sh
@@ -1,3 +1,4 @@
 mkdir -p /root/hasaki/stathub
 wget https://service-1253872964.cos.ap-guangzhou.myqcloud.com/stathub -P /root/hasaki/stathub/
+chmod 755 /root/hasaki/stathub/stathub
 /root/hasaki/stathub/stathub -c /root/hasaki/stathub/stathub.conf --init-server
